e6b20e9b355859af0b6d3cf03352fc61a4fae39b
dimas subaktianto <dimas.subaktianto11@gmail.com>
2020-02-17 10:07:00 +0700

    modify some element

---
+++ application/views/back/template/foot.php
@@ -0,0 +1,31 @@
+
+            <footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
+        </div>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="<?= base_url()?>assets/back/modules/jquery.min.js"></script>
+    <script src="<?= base_url()?>assets/back/modules/popper.js"></script>
+    <script src="<?= base_url()?>assets/back/modules/tooltip.js"></script>
+    <script src="<?= base_url()?>assets/back/modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= base_url()?>assets/back/modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="<?= base_url()?>assets/back/modules/moment.min.js"></script>
+    <script src="<?= base_url()?>assets/back/js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="<?= base_url()?>assets/back/js/scripts.js"></script>
+    <script src="<?= base_url()?>assets/back/js/custom.js"></script>
+</body>
+
+</html>
